b11d7dacbb7b11e36930997167c564ee0b90b736
Patrick McGreevy & Peter Tinti <student@epicodus.com>
2014-03-03 15:42:15 -0800

    refact to use only iterators and not loops

--- lib/pig_latin.rb
+++ lib/pig_latin.rb
@@ -6,25 +6,27 @@ end
 def to_pig_latin(word) 
   result = ''
   consonant_sequ = ''
+  previous_letter = ''
+  vowel_caught = false
   capital = (word =~ /[A-Z]/)
   word.downcase!
 
-  for i in 0..(word.length-1)
-    if word[i] == 'u' && i >0 && word[i-1] == 'q'
-      consonant_sequ += word[i]
-      next
+  word.each_char do |letter|
+    if !vowel_caught
+      if letter == 'u' && previous_letter == 'q'
+        consonant_sequ += letter
+      elsif vowel?(letter)
+        vowel_caught = true
+      elsif letter == 'y' && previous_letter != ''
+        vowel_caught = true
+      else
+        consonant_sequ += letter
+      end
     end
-    if vowel?(word[i])
-      initial_vowel_index = i
-      break
-    elsif word[i] == 'y' && i > 0
-      initial_vowel_index = i
-      break
-    end
-    consonant_sequ += word[i]
+    previous_letter = letter
   end
 
-  wordStart = word[initial_vowel_index..word.length-1]
+  wordStart = word[consonant_sequ.length..word.length-1]
   if capital == 0
     wordStart.capitalize!
   end
@@ -35,15 +37,15 @@ def pig_latin(string)
   result = ''
   word_array = string.scan(/([a-zA-Z']+)([^a-zA-Z']*)/)
 
-  word_array.each {|pair|
-    pair.each {|element|
+  word_array.each do |pair|
+    pair.each do |element|
       if element =~ /[a-z]/i
         result += to_pig_latin(element)
       else
         result += element
       end
-    }
-  }
+    end
+  end
 
   result
 end
